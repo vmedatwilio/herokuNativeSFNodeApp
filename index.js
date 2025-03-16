fc02efa1738753483db387bbaaf3ec500c4191dc
venkata phani raju meda <vmeda@twilio.com>
2025-03-16 13:00:54 +0530

    Added log for request body

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
