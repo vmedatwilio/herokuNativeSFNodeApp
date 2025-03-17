c2c10595e1814d11389799dd8b1aa7e59a88a116
hran <hran@mail.com>
2025-03-17 21:18:13 +0530

    added logic to getValueByKey

--- index.js
+++ index.js
@@ -373,8 +373,9 @@ async function fetchRecords(conn, queryOrUrl, groupedData = {}, isFirstIteration
         console.error(`Error fetching activities: ${error.message}`);
         throw error;
     }
-    function getValueByKey(records, searchKey) {
-        const record = records.find(item => item.key === searchKey);
-        return record ? record.value : null;
-    }
+    
+}
+function getValueByKey(records, searchKey) {
+    const record = records.find(item => item.key === searchKey);
+    return record ? record.value : null;
 }
